281e9dc75ffe7a3c6ea06b192e1f4add75e5de37
Mustapha <misa@brandeis.edu>
2013-10-30 09:58:20 -0400

    Plot the points on the images

--- smart_thumbnail_scripts/testSmarterThumbnail.m
+++ smart_thumbnail_scripts/testSmarterThumbnail.m
@@ -4,13 +4,13 @@
 % script.
 
 % Get the vector containing names of images
-nameVec = getFileNames( 1 );
+nameVec = getFileNames( 2 );
 
 % Loop through and make images smart_thumbnails
 for i = 1:102
     
     name = nameVec(1,i);
     filename = strcat('thumbnail_', num2str(i));
-    makeSmarter( name{1}, filename, 1, 0 );
+    makeSmarter( name{1}, filename, 2, 0 );
     
 end
